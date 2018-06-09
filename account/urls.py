d4185b3cdfa6268e3d9882e52892df8fc9894673
Yehuda Deutsch <yeh@uda.co.il>
2018-06-09 12:54:03 +0300

    Use class based views from account

--- account/urls.py
+++ account/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
 from django.contrib.auth.views import (
-    password_change,
-    password_change_done,
-    password_reset,
-    password_reset_done,
-    password_reset_confirm,
-    password_reset_complete,
     LoginView,
     LogoutView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
 )
 
 from .views import AccountView
@@ -21,13 +21,13 @@ urlpatterns = [
 
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
-    url(r'^password/change/$', password_change, name='password-change',
+    url(r'^password/change/$', PasswordChangeView.as_view(), name='password-change',
         kwargs={'post_change_redirect': 'account:password-change-done'}),
-    url(r'^password/change/done/$', password_change_done, name='password-change-done'),
-    url(r'^password/reset/$', password_reset, name='password-reset',
+    url(r'^password/change/done/$', PasswordChangeDoneView.as_view(), name='password-change-done'),
+    url(r'^password/reset/$', PasswordResetView.as_view(), name='password-reset',
         kwargs={'post_reset_redirect': 'account:password-reset-done'}),
-    url(r'^password/reset/done/$', password_reset_done, name='password-reset-done'),
-    url(r'^password/reset/confirm/$', password_reset_confirm, name='password-reset-confirm',
+    url(r'^password/reset/done/$', PasswordResetDoneView.as_view(), name='password-reset-done'),
+    url(r'^password/reset/confirm/$', PasswordResetConfirmView.as_view(), name='password-reset-confirm',
         kwargs={'post_reset_redirect': 'account:password-reset-complete'}),
-    url(r'^password/reset/complete/$', password_reset_complete, name='password-reset-complete'),
+    url(r'^password/reset/complete/$', PasswordResetCompleteView.as_view(), name='password-reset-complete'),
 ]
